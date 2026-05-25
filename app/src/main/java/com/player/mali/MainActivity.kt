ec83c97b7f51bcb901c70ed7def75353b7c42455
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-25 15:50:09 +0100

    Add files via upload

--- app/src/main/java/com/player/mali/MainActivity.kt
+++ app/src/main/java/com/player/mali/MainActivity.kt
@@ -1,1 +1,21 @@
+package com.player.mali
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import com.player.mali.ui.MaliApp
+import com.player.mali.viewmodel.MaliViewModel
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: MaliViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaliApp(viewModel = viewModel)
+        }
+    }
+}
